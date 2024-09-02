91c266595049758a3a79cea8a38ebdca06c512ab
yoshimogura <[200~nakatayoshifumi3@gmail.com~>
2024-09-02 17:50:38 +0900

    AddForce Z

--- Assets/Scenes/putter.cs
+++ Assets/Scenes/putter.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class putter : MonoBehaviour
 {
     // Start is called before the first frame update
+    float shotpower=90; 
+    public Vector3 torque = new Vector3(5f, 0f, 0f);
     void Start()
     {
-        
+      
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-    Transform myTransform = this.transform;
-    Vector3 worldAngle = myTransform.eulerAngles;
-         
     if(Input.GetKeyDown(KeyCode.Space))
       {
-       transform.Rotate(new Vector3(100,0,0));
+         Rigidbody rb = GetComponent<Rigidbody>();   //取得してる
+            rb.AddForce(torque,ForceMode.Acceleration);
       }
     }
 
