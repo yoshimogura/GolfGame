395fc378b9a35e3aca616876d5e2b1d50ed0f68b
yoshimogura <[200~nakatayoshifumi3@gmail.com~>
2024-09-16 16:10:25 +0900

    2投目　X座標の差を使ってやろうとしてる

--- Assets/Scenes/camera.cs
+++ Assets/Scenes/camera.cs
@@ -8,15 +8,30 @@ public class camera : MonoBehaviour
     GameObject boll;
     void Start()
     {
+
         this.boll=GameObject.Find("ボール (1)");
         
     }
     // Update is called once per frame
     void Update()
-    {
+    {   
+        
         if(-14>this.boll.transform.position.x){
+        GameObject camera = GameObject.Find("Main Camera");
+            if (camera)
+            {
+                
+                Debug.Log("camera exist");
+                camera.transform.position = new Vector3(81, 20, -57);
+            }
+            else
+            {
+                Debug.Log("camera not exist");
+            }
+        }else{
+            GameObject camera = GameObject.Find("Main Camera");
+            camera.transform.position = new Vector3(158, 32, -57);
 
-            Debug.Log("camera");
         }
 
 
--- Assets/Scenes/putter.cs
+++ Assets/Scenes/putter.cs
@@ -2,32 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class putter : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float shotpower=-50; 
     
-    void Start()
+    public float shotpower=-50; 
+    public Rigidbody rb;
+    public string permit="false";
+    float x=-1;
+    float Memorization=0;
+    string firstshot ="YES";
+
+    void FixedUpdate()
     {
       
-    
+      
+        
+         
     }
+    
 
-    // Update is called once per frame
     void Update()
     {
-    if(Input.GetKeyDown(KeyCode.Space))
+      
+    // if(Input.GetKeyDown(KeyCode.Space))
+    if(Input.GetKeyDown(KeyCode.Space)&&permit=="true")
       {
+        permit="false";
+        if(firstshot=="YES"){
+         firstshot ="NO";
+        }
          Rigidbody rb = GetComponent<Rigidbody>();   //取得してる
             rb.AddForce(shotpower,0,0,ForceMode.VelocityChange);
+            
+            
       }
       if(this.transform.position.y < 9.4)
       {
-        this.transform.position = new Vector3(38, 14, -58);
+        this.transform.position = new Vector3(129, 14, -58);
       }
 
+
+
+
+
+      Debug.Log(x-Memorization  );
+      Debug.Log(permit);
+      Debug.Log(firstshot);
+        // permit = rb.IsSleeping() ? "true" : "false";
+        if(firstshot=="NO"){
+          x=this.transform.position.x;
+        }
+        if(x-Memorization<=0.001){
+          permit="true";
+        }
+        if(firstshot=="NO"){
+         Memorization=x;
+        }
     }
 
 }
