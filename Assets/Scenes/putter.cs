65a362fd2992f5ed6bfe648b6af674060bd24324
yoshimogura <[200~nakatayoshifumi3@gmail.com~>
2024-10-07 17:45:41 +0900

    magnitudeを使ってみた

--- Assets/Scenes/putter.cs
+++ Assets/Scenes/putter.cs
@@ -5,43 +5,43 @@ using UnityEngine;
 
 public class putter : MonoBehaviour
 {
+
+    public float forceAmount = 3f; // ボールに与える力の量
+    public float speedThreshold = 2.9f; // ボールを止めるための速度の閾値
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     
     public float shotpower=-1; 
-    public Rigidbody rb;
-    bool moving=false;
 
+  
 
 
     void Start(){
-      
-      rb = GetComponent<Rigidbody>();   //取得してる
-    }
-    void FixedUpdate()
-    {
-      
-     
-        if(rb.IsSleeping()){
-        moving=true;
-      }else{
-        moving=false;
-      
-      }
+        rb = GetComponent<Rigidbody>();
+
+        // 最初に力を加える
+        rb.AddForce(transform.forward * forceAmount, ForceMode.Impulse);
+        Debug.Log(transform.forward);
     }
     
 
     void Update()
     {
-    if(Input.GetKeyDown(KeyCode.Space)&&moving==!false)
+    // ボールの速度が閾値を下回ったら
+        if (rb.velocity.magnitude < speedThreshold)
+        {
+            // ボールを完全に止める
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            // Rigidbodyの物理計算を停止して完全に静止させる
+            rb.isKinematic = true;
+            Debug.Log("stop");
+        }
+      if(this.transform.position.y < 10)
       {
-        //  Rigidbody rb = GetComponent<Rigidbody>();   //取得してる
-            rb.AddForce(shotpower,0,0,ForceMode.VelocityChange);
-            
-            
-      }
-      if(this.transform.position.y < 9.4)
-      {
-        this.transform.position = new Vector3(129, 14, -58);
+        this.transform.position = new Vector3(129, 15, -58);
       }
 
 
