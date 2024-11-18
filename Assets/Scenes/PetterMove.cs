0a5ca511be8fbfd15f127acfaa3c88d171489307
yoshimogura <[200~nakatayoshifumi3@gmail.com~>
2024-11-18 17:26:50 +0900

    自動フォーマットの追加

--- Assets/Scenes/PetterMove.cs
+++ Assets/Scenes/PetterMove.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class PetterMove : MonoBehaviour
 {
-   public float speed = 5.0f; // 振り子の速度
-   public float maxAngle = 90.0f; // 振り子の最大角度 
-   private ConfigurableJoint joint; 
-   void Start() { joint = GetComponent<ConfigurableJoint>(); 
-   JointDrive drive = new JointDrive { positionSpring = 1000, positionDamper = 100, maximumForce = Mathf.Infinity }; 
-   joint.angularXDrive = drive; 
-  } void Update() { 
-    float angle = maxAngle * Mathf.Sin(Time.time * speed); 
+  public float speed = 5.0f; // 振り子の速度
+  public float maxAngle = 90.0f; // 振り子の最大角度 
+  private ConfigurableJoint joint;
+  void Start()
+  {
+    joint = GetComponent<ConfigurableJoint>();
+    JointDrive drive = new JointDrive { positionSpring = 1000, positionDamper = 100, maximumForce = Mathf.Infinity };
+    joint.angularXDrive = drive;
+  }
+  void Update()
+  {
+    float angle = maxAngle * Mathf.Sin(Time.time * speed);
     joint.targetRotation = Quaternion.Euler(angle, 0, 0);
   }
 }
